84300849bf976a70f62904a19de99ea620d34994
earivalagan <earivalagan@indeed.com>
2021-07-25 23:41:57 -0700

    tool tip and hoverover events

--- Update Firestore/graph.js
+++ Update Firestore/graph.js
@@ -9,25 +9,10 @@ const graph = canvas.append('g').attr('transform', `translate(${cent.x},${cent.y
 // pie(data) function always takes array like input data as the argument 
 // and returns an array objects with startAngle and endAngle, etc - one for each data point
 
-const computeAngles = d3.pie() //sets key variables - value, startAngle, etc and returns pie(data) obj
-     .value(d=> d.cost) //sets the "value" variable and returns pie(data)
-     .sort(null) //sets the "sort" variable and returns pie(data) object.
+const computeAngles = d3.pie() 
+     .value(d=> d.cost) 
+     .sort(null) 
 // computeAngles is the result of d3.pie().value(d=>d.cost).sort(null), which is pie(data)
-
-// When value(d=>d.cost) executes. It changes the variable "value", which
-// was holding a generic identify function. Now "value" will have d=>d.cost instead.
-
-// Since the above code alters the variables "value" & "sort" in memory
-// and returns the function pie(data) code, computeAngles is not of much use.
-// You still need to invoke it using () operator.
-
-// pie(data) needs an argument. So, you should pass an arg to invoke the funcion computeAngles.
-// computeAngles(data) is same as d3.pie().value(d=>d.cost).sort(null)(data)
-// This time pie(data) is executed, which returns an array
-
-// The main reason for confusion is that JS allows a regular function to behave like an object
-// hence functions can have their own properties and methods. 
-// here pie(data) has methods like value(), sort(), etc
 
 // unlike d3.pie(), d3.arc() only takes one data element (no arrays allowed)
 // and returns one "d" value as a string
@@ -36,47 +21,76 @@ const arcPath = d3.arc()
     .outerRadius(dims.radius)
     .innerRadius(dims.radius/2);
 
+// Add tool tip - https://cdnjs.com/libraries/d3-tip
+const tip = d3.tip()
+    .attr('class', 'tip')
+    .html(d => {
+        let content = `<div class="name">${d.data.name}</div>`;
+        content += `<div class="cost">${d.data.cost}</div>`;
+        return content;
+    });
+
+graph.call(tip);
+
 const update = (data) => {
+
     //Build a scale to create colors for the pie slices
-    
     pieColor = d3.scaleOrdinal()
                 .domain(data.map(d=>d.name))
                 .range(d3['schemeSet1'])
     
     //d3.selectAll() executes basic JS to select elements and return Selection$1 object
-    //Selection$1 object has methods like, append, attr, enter, exit, etc.
-    //Every method of Selection$1 object executes some JS code and returns Selection$1 object
-
     const paths = graph.selectAll('path') 
          .data(computeAngles(data)); 
     //data() returns an object that conatins three sub objects - entry, exit, groups & parents
-    //entry object holds an array that contains the input data in it's entrity.
 
     paths.enter()
         .append('path')
             .attr('class', 'arc')
-            .attr('d', d=>arcPath(d)) 
-            .attr('stroke', '#fff')
+            .attr('stroke', 'black')
             .attr('stoke-width', 3)
             .attr('fill', d=>pieColor(d.data.name))//actual data is inside the object called "data"
+            .transition().duration(750)
+                .attrTween('d', arcTweenEnter)
 
-    //attr() takes two arguments. First is always a string, which represents the HTML attribute
-    //The second can be a string or a function. If it is a function, attr() method runs a 
-    //loop for each data element and applys the provided function. Hence the function we provide
-    //is always for individual data point, not for all data points.
-
-    //Update selection when we update the data
-    paths.attr('d', d=>archPath(d))
+    //Update selection. When we update the data, we just need to update d
+    paths.attr('d', d=>arcPath(d))
 
     //Exit selection when we delete a data in the input
-    paths.exit().remove()
+    paths.exit()
+        .transition().duration(750)
+            .attrTween('d', arcTweenExit)
+        .remove()
     
-    //add text
+    //add text (better add a group and add text & path to the group)
     paths.enter()
     .append("text")
         .text(d=>d.data.name)
         .attr("x", d=>arcPath.centroid(d)[0])
         .attr("y", d=>arcPath.centroid(d)[1])
+
+    //update text
+    //exti text
+
+
+    // basic event listner for all path elements
+    // graph.selectAll('path')
+    //     .on('mouseover', handleMouseOver) 
+    //     .on('mouseout', handleMouseOut) //handleMouseOver automatically gets d, i & n
+        //d is the data of the element, n is the list of all paths. i is the index of the current element
+        // .on('click', handleClick)
+
+    //event listner that shows both the tooltip and the color change
+    graph.selectAll('path')
+        .on('mouseover', (d,i,n)=>{
+            tip.show(d, n[i]);
+            handleMouseOver(d,i,n);
+        }) 
+        .on('mouseout', (d,i,n)=>{
+            tip.hide();
+            handleMouseOut(d,i,n);
+        }) 
+        .on('click', handleClick)
 };
 
 var data = [];
@@ -107,3 +121,42 @@ db.collection('expenses').onSnapshot(res => {
     update(data);
 
 });
+
+//it calculates attribute "d" over time "t". 
+//First the start angle is set as the end angle. 
+//For each value of "t", the function i(t) returns the d value
+const arcTweenEnter = (d) => {
+    var i = d3.interpolate(d.endAngle, d.startAngle);
+
+    return function(t){
+        d.startAngle = i(t);
+        return arcPath(d);
+    }
+}
+const arcTweenExit = (d) => {
+    var i = d3.interpolate(d.startAngle, d.endAngle);
+
+    return function(t){
+        d.startAngle = i(t);
+        return arcPath(d);
+    }
+}
+
+//Event Handlers. upto v5 (d,i,n) are the arguments. V6 and up only has 2 orgs (e,d)
+//Mouse Over
+const handleMouseOver = function (d, i ,n) { 
+    d3.select(this)
+        .transition().duration(300)
+            .attr('fill', 'white')
+}
+//Mouse Out
+const handleMouseOut = function (d, i ,n) {
+    d3.select(this)
+        .transition().duration(300)
+            .attr('fill', pieColor(d.data.name))
+}
+//Delete data point on a Click
+const handleClick = function (d, i ,n) {
+    const id = d.data.id;
+    db.collection('expenses').doc(id).delete();
+}
